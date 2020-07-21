f164a15f94c402cee72bcdc53d69adff259e9e7e
dora <31735614+dora1998@users.noreply.github.com>
2020-07-22 04:01:08 +0900

    lodash-esに入れ替え

--- src/domains/blog_list/components/BlogCardList/BlogCardList.jsx
+++ src/domains/blog_list/components/BlogCardList/BlogCardList.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import _ from 'lodash';
+import {map, chunk} from 'lodash-es';
 
 import { BlogCard } from '../BlogCard';
 
 export function BlogCardList({ list, columnCount }) {
-  const rows = _.chunk(list, columnCount);
+  const rows = chunk(list, columnCount);
 
   return (
     <div className="blog-list-BlogCardList">
-      {_.map(rows, (rowItems, i) => (
+      {map(rows, (rowItems, i) => (
         <div key={i} className="blog-list-BlogCardList__row">
-          {_.map(rowItems, (item, j) => (
+          {map(rowItems, (item, j) => (
             <div
               key={j}
               className="blog-list-BlogCardList__column"
